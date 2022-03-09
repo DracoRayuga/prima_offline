a8f7b0cbf574cb2efc42311ee99eab47a6381ab3
Roystan Dsilva <dsilvaroystan28@gmail.com>
2022-03-09 04:16:47 +0000

    application added

--- prima/webapp/controller/App.controller.js
+++ prima/webapp/controller/App.controller.js
@@ -10,6 +10,23 @@ sap.ui.define([
         return Controller.extend("opensap.prima.controller.App", {
             onInit: function () {
 
+            },
+            onUploadAttachmentDialogCancel: function (oEvent) {
+			    this.oDialog.destroy();
+		    },
+            onupload: function(oEvent){
+                var that = this;
+                var oView = this.getView();
+                that.oDialog = oView.byId("uploadfragment");
+						if (!that.oDialog) {
+							// create dialog via fragment factory
+							that.oDialog = sap.ui.xmlfragment(oView.getId(), "opensap.prima.fragment.upload", that);
+							oView.addDependent(that.oDialog);
+						}
+						that.oDialog.open();
+            },
+            onUploadfilethroughodatav4: function(oEvent){
+                
             }
         });
     });
